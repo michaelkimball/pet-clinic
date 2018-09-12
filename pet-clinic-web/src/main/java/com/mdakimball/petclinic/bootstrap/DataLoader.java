d2ad729b58709660493c0f0c82bf929ae0e6c92a
Michael Kimball <m.d.a.kimball@gmail.com>
2018-09-12 15:54:48 -0400

    updating map implementation to manage id generation. closes #23

--- pet-clinic-web/src/main/java/com/mdakimball/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/mdakimball/petclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Vince");
         owner1.setLastName("Reynolds");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Alice");
         owner2.setLastName("Adair");
 
@@ -38,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Camille");
         vet1.setLastName("Belden");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Nick");
         vet2.setLastName("Campbell");
 
