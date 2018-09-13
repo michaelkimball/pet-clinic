1afe33a4976e9cf0a774a68c1f2f4e7f95ae076c
Michael Kimball <m.d.a.kimball@gmail.com>
2018-09-13 17:25:59 -0400

    added spring data jpa for pet. closes #43

---
+++ pet-clinic-data/src/main/java/com/mdakimball/petclinic/services/springdatajpa/PetServiceSpringDataJPA.java
@@ -0,0 +1,45 @@
+package com.mdakimball.petclinic.services.springdatajpa;
+
+import com.mdakimball.petclinic.model.Pet;
+import com.mdakimball.petclinic.repositories.PetRepository;
+import com.mdakimball.petclinic.services.PetService;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class PetServiceSpringDataJPA implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetServiceSpringDataJPA(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        return new HashSet<>((Collection<Pet>) petRepository.findAll());
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
